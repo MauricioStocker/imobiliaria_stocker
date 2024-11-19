0d41caad86bc1e3ef6bac134190bd7d4df2eaecd
Mauricio Stocker <caicara1010@gmail.com>
2024-11-19 14:52:29 -0300

    Commit inicial com README

---
+++ src/test/java/imobiliaria_stocker/com/imobiliaria/ImobiliariaApplicationTests.java
@@ -0,0 +1,13 @@
+package imobiliaria_stocker.com.imobiliaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImobiliariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
